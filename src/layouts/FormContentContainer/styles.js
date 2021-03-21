5966e6794c5a9e40e3c87bdb6a9e3d0be8336e8b
irwinarruda <arruda.irwin@gmail.com>
2021-03-21 17:58:16 -0300

    minor section fix

--- src/layouts/FormContentContainer/styles.js
+++ src/layouts/FormContentContainer/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.header`
+export const Container = styled.section`
     max-width: 620px;
     width: 100%;
     margin-top: 15px;
