1e4a184949d6e7c5b436aa908b173573aa5a9f4f
Irwin Arruda <arruda.irwin@gmail.com>
2022-08-24 21:42:10 -0300

    Ajustada validação com novos campos

--- src/helpers/formValidationHandler.js
+++ src/helpers/formValidationHandler.js
@@ -9,14 +9,18 @@ formValidations.set('5', validateInputRadioField);
 formValidations.set('6', validateSelect);
 formValidations.set('8', validateInputTextField);
 formValidations.set('9', validateInputTextSearch);
+formValidations.set('10', validateInputNumberField);
+formValidations.set('11', validateInputNumber);
 
 export function makeValidationSchema(data, blockedFields) {
     const validationSchema = {};
     data.forEach((formItem) => {
         if (blockedFields.indexOf(formItem.id_pergunta) === -1) {
-            validationSchema[
-                `form_${formItem.id_pergunta}`
-            ] = formValidations.get(formItem.tipo)(formItem);
+            if (formValidations.get(formItem.tipo)) {
+                validationSchema[
+                    `form_${formItem.id_pergunta}`
+                ] = formValidations.get(formItem.tipo)(formItem, blockedFields);
+            }
         }
     });
     return Yup.object().shape(validationSchema);
@@ -29,6 +33,9 @@ export function validateInputCheckboxField(formItem) {
     Yup.string().required('Este campo deve ser preenchido');
 }
 export function validateInputText(formItem) {
+    return Yup.string().required('Este campo deve ser preenchido');
+}
+export function validateInputNumber(formItem) {
     return Yup.string().required('Este campo deve ser preenchido');
 }
 export function validateTextArea(formItem) {
@@ -46,13 +53,26 @@ export function validateInputRadioField(formItem) {
 export function validateSelect(formItem) {
     return Yup.string().required('Este campo deve ser preenchido');
 }
-export function validateInputTextField(formItem) {
+export function validateInputTextField(formItem, blockedFields) {
     const res_obj = {};
-    formItem.itens.forEach((item) => {
-        res_obj[`res_${item.id_pergunta}`] = Yup.string().required(
-            'Este campo deve ser preenchido',
-        );
-    });
+    formItem.itens
+        .filter((item) => !blockedFields.includes(item.id_pergunta))
+        .forEach((item) => {
+            res_obj[`res_${item.id_pergunta}`] = Yup.string().required(
+                'Este campo deve ser preenchido',
+            );
+        });
+    return Yup.object().shape(res_obj);
+}
+export function validateInputNumberField(formItem, blockedFields) {
+    const res_obj = {};
+    formItem.itens
+        .filter((item) => !blockedFields.includes(item.id_pergunta))
+        .forEach((item) => {
+            res_obj[`res_${item.id_pergunta}`] = Yup.string().required(
+                'Este campo deve ser preenchido',
+            );
+        });
     return Yup.object().shape(res_obj);
 }
 export function validateInputTextSearch(formItem) {
